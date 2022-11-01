5a01dbe28da67d9377585ec8582d4510c09d7a9b
levi <levihoang@github.com>
2022-11-01 16:36:52 +0700

    hotfix: use option_button

--- lib/src/features/button/screens/button_screen.dart
+++ lib/src/features/button/screens/button_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gallery_app/src/config/constants/app_colors.dart';
+import 'package:gallery_app/src/features/button/widgets/toggle_button.dart';
 
 import '../widgets/half_circle.dart';
 
@@ -31,112 +32,117 @@ class _ButtonScreenState extends State<ButtonScreen> {
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: double.infinity,
-                child: DropdownButton<String>(
-                    hint: const Text('DropDownButton'),
+        child: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton<String>(
+                      hint: const Text('DropDownButton'),
+                      items: dropDownItems
+                          .map((String e) => DropdownMenuItem<String>(
+                              value: e, child: Text(e)))
+                          .toList(),
+                      // value: dropdownItem,
+                      onChanged: (value) {}),
+                ),
+                DropdownButtonFormField(
+                    hint: const Text('DropdownButtonFormField'),
                     items: dropDownItems
                         .map((String e) =>
                             DropdownMenuItem<String>(value: e, child: Text(e)))
                         .toList(),
-                    // value: dropdownItem,
                     onChanged: (value) {}),
-              ),
-              DropdownButtonFormField(
-                  hint: const Text('DropdownButtonFormField'),
-                  items: dropDownItems
-                      .map((String e) =>
-                          DropdownMenuItem<String>(value: e, child: Text(e)))
-                      .toList(),
-                  onChanged: (value) {}),
-              const Icon(
-                CupertinoIcons.plus_app_fill,
-                size: 30,
-              ),
-              SizedBox(
+                const Icon(
+                  CupertinoIcons.plus_app_fill,
+                  size: 30,
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('Elevated Button'))),
+                SizedBox(
                   width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: () {}, child: const Text('Elevated Button'))),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.person),
-                    label: const Text('Elevated Button Icon')),
-              ),
-              SizedBox(
+                  child: ElevatedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Elevated Button Icon')),
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: TextButton(
+                        onPressed: () {}, child: const Text('Text Button'))),
+                SizedBox(
                   width: double.infinity,
-                  child: TextButton(
-                      onPressed: () {}, child: const Text('Text Button'))),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.person),
-                    label: const Text('Text Button Icon')),
-              ),
-              SizedBox(
+                  child: TextButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Text Button Icon')),
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                        onPressed: () {},
+                        child: const Text('Outlined Button'))),
+                SizedBox(
                   width: double.infinity,
-                  child: OutlinedButton(
-                      onPressed: () {}, child: const Text('Outlined Button'))),
-              SizedBox(
-                width: double.infinity,
-                child: OutlinedButton.icon(
+                  child: OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: const Icon(Icons.person),
+                      label: const Text('Outlined Button Icon')),
+                ),
+                CupertinoButton(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+                    child: Container(
+                        height: 40,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: AppColors.grey,
+                            borderRadius: BorderRadius.circular(10)),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'Cupertino Button',
+                          style: TextStyle(color: AppColors.white),
+                        )),
+                    onPressed: () {}),
+                MaterialButton(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
                     onPressed: () {},
-                    icon: const Icon(Icons.person),
-                    label: const Text('Outlined Button Icon')),
-              ),
-              CupertinoButton(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                  child: Container(
-                      height: 40,
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                          color: AppColors.grey,
-                          borderRadius: BorderRadius.circular(10)),
-                      alignment: Alignment.center,
-                      child: const Text(
-                        'Cupertino Button',
-                        style: TextStyle(color: AppColors.white),
-                      )),
-                  onPressed: () {}),
-              MaterialButton(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-                  onPressed: () {},
-                  child: CustomPaint(
+                    child: CustomPaint(
+                        painter: ShapePainter(),
+                        child: Container(
+                            height: 40,
+                            width: double.infinity,
+                            alignment: Alignment.center,
+                            child: const Text('Material Button')))),
+                RawMaterialButton(
+                    onPressed: () {},
+                    child: CustomPaint(
                       painter: ShapePainter(),
                       child: Container(
                           height: 40,
                           width: double.infinity,
                           alignment: Alignment.center,
-                          child: const Text('Material Button')))),
-              RawMaterialButton(
+                          child: const Text('Raw material Button')),
+                    )),
+                // Row(
+                //   children: [
+                //     customRadioButton('Option 1', 0),
+                //     customRadioButton('Option 2', 1),
+                //     customRadioButton('Option 3', 2),
+                //   ],
+                // ),
+                const ToggleButtonWidget(),
+                FloatingActionButton(
                   onPressed: () {},
-                  child: CustomPaint(
-                    painter: ShapePainter(),
-                    child: Container(
-                        height: 40,
-                        width: double.infinity,
-                        alignment: Alignment.center,
-                        child: const Text('Raw material Button')),
-                  )),
-              Row(
-                children: [
-                  customRadioButton('Option 1', 0),
-                  customRadioButton('Option 2', 1),
-                  customRadioButton('Option 3', 2),
-                ],
-              ),
-              FloatingActionButton(
-                onPressed: () {},
-                child: const Icon(CupertinoIcons.plus),
-              )
-            ]),
+                  child: const Icon(CupertinoIcons.plus),
+                )
+              ]),
+        ),
       ),
     );
   }
